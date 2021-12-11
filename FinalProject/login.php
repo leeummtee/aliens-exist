ad1782f6464fbf7fef04fa7fb17b2d7807d7336e
Fiona <10bit1023a@gmail.com>
2021-12-10 23:46:01 -0800

    push

--- FinalProject/login.php
+++ FinalProject/login.php
@@ -10,12 +10,14 @@ if(isset($_POST['login']))
 {
     $user = $_POST['username'];
     $pass = $_POST['password'];
+
     $result = $conn -> query("SELECT username FROM member WHERE username = '$user' AND password = '$pass'");
 
     if($result -> num_rows > 0)
     {
       $_SESSION['isLoggedIn'] = 1;
       $_SESSION['username'] = $user;
+
       exit('success');
     } else {
       exit('fail');
