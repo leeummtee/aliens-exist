355efba37d009d083930ed83ea606ac916ca4c56
Fiona <10bit1023a@gmail.com>
2021-12-11 17:03:38 -0800

    asdasdasdsadsadsadsad

--- FinalProject/home.php
+++ FinalProject/home.php
@@ -1,7 +1,167 @@
 <!DOCTYPE HTML>
 <!--I used P02 as reference -->
 <html lang="en">
+<?php
+include_once("database.php");
+session_start();
 
+$sql_statement = "";
+function defaultSetting()
+{
+  return "SELECT * FROM entries ORDER BY dateposted DESC LIMIT 6";
+}
+
+if(isset($_SESSION['isLoggedIn']) && $_SESSION['isLoggedIn'] == 1)
+{
+  $user = $_SESSION['username'];
+  $stmt = $conn->prepare("SELECT EXISTS(
+         SELECT *
+         FROM set_pref
+         WHERE username = ?)");
+  $stmt->bind_param("s", $user);
+  $stmt->execute();
+  $result = $stmt->get_result();
+  $prefExists = "0";
+  while ($row = $result->fetch_array()) {
+      foreach ($row as $r) {
+          if($r == 0){
+            $prefExists = "0";
+          }
+          else {
+            $prefExists = "1";
+          }
+      }
+  }
+  if($prefExists == "1")
+  {
+    $user = $_SESSION['username'];
+    $stmt_get_pref = $conn->prepare("SELECT country, city, shape, upvotes FROM
+    preferences INNER JOIN set_pref ON preferences.preference_id = set_pref.preference_id
+    WHERE username =  ?");
+    $stmt_get_pref->bind_param("s", $user);
+
+    $stmt_get_pref->execute();
+    $city = '';
+    $country = '';
+    $shape = '';
+    $upvotes = '';
+
+    $result = $stmt_get_pref->get_result();
+    while ($row = $result->fetch_array()) {
+        $city = $row['city'];
+        $country = $row['country'];
+        $shape = $row['shape'];
+        $country = $row['upvotes'];
+      }
+    $sql_statement = "SELECT * FROM entries";
+    if($shape != '')
+    {
+      $sql_statement .= $shape . " ORDER BY dateposted DESC LIMIT 6";
+      $result_records = $conn->query($sql_statement);
+      echo "<h1> Shapes </h1>";
+      if ($result_records->num_rows > 0) {
+        while($row = $result_records->fetch_assoc()) {
+          echo "<br><br><a href=" . "'?link=" . $row["entryid"] . "'" . "<h3>Date Posted:" . $row["dateposted"]."</h3></a><br>";
+          if(isset($_GET['link'])){
+            //echo "uh";
+            $_SESSION['entryid'] = $_GET['link'];
+            header("Location: details.php");
+          }
+        }
+      }
+    }
+    if($country != '')
+    {
+      $stmt = $conn->prepare("SELECT * FROM entries WHERE country = ? ORDER BY dateposted DESC LIMIT 6");
+      $stmt->bind_param("s", $country);
+      $stmt->execute();
+      $results = $stmt->get_result();
+      echo "<h1> Country </h1>";
+      if($results != null && $results->num_rows > 0){
+        if ($results->num_rows > 0) {
+          while($row = $results->fetch_assoc()) {
+            echo "<br><br><a href=" . "'?link=" . $row["entryid"] . "'" . "<h3>Date Posted:" . $row["dateposted"]."</h3></a><br>";
+            if(isset($_GET['link'])){
+              //echo "uh";
+              $_SESSION['entryid'] = $_GET['link'];
+              header("Location: details.php");
+            }
+          }
+        }
+      } else {
+         echo "No results were found </br>";
+      }
+    }
+
+    if($city != '')
+    {
+      $stmt2 = $conn->prepare("SELECT * FROM entries WHERE city = ? ORDER BY dateposted DESC LIMIT 6");
+      $stmt2->bind_param("s", $city);
+      $stmt2->execute();
+      $result = $stmt2->get_result();
+      echo "<h1> City </h1>";
+      if($result != null && $result->num_rows > 0){
+        if ($result->num_rows > 0) {
+          while($row = $result->fetch_assoc()) {
+            echo "<br><br><a href=" . "'?link=" . $row["entryid"] . "'" . "<h3>Date Posted:" . $row["dateposted"]."</h3></a><br>";
+            if(isset($_GET['link'])){
+              //echo "uh";
+              $_SESSION['entryid'] = $_GET['link'];
+              header("Location: details.php");
+            }
+          }
+        }
+      } else {
+         echo "No results were found </br>";
+      }
+    }
+    if($city == '' && $country == '' && $shape == '')
+    {
+      $sql_statement = defaultSetting();
+      $result_records = $conn->query($sql_statement);
+      echo "<h1> Newest Posts </h1>";
+      if ($result_records->num_rows > 0) {
+        while($row = $result_records->fetch_assoc()) {
+          echo "<br><br><a href=" . "'?link=" . $row["entryid"] . "'" . "<h3>Date Posted:" . $row["dateposted"]."</h3></a><br>";
+          if(isset($_GET['link'])){
+            //echo "uh";
+            $_SESSION['entryid'] = $_GET['link'];
+            header("Location: details.php");
+          }
+        }
+      }
+    }
+  } else {
+    $sql_statement = defaultSetting();
+    $result_records = $conn->query($sql_statement);
+    echo "<h1> Newest Posts </h1>";
+    if ($result_records->num_rows > 0) {
+      while($row = $result_records->fetch_assoc()) {
+        echo "<br><br><a href=" . "'?link=" . $row["entryid"] . "'" . "<h3>Date Posted:" . $row["dateposted"]."</h3></a><br>";
+        if(isset($_GET['link'])){
+          //echo "uh";
+          $_SESSION['entryid'] = $_GET['link'];
+          header("Location: details.php");
+        }
+      }
+    }
+  }
+} else {
+  $sql_statement = defaultSetting();
+  $result_records = $conn->query($sql_statement);
+  echo "<h1> Newest Posts </h1>";
+  if ($result_records->num_rows > 0) {
+    while($row = $result_records->fetch_assoc()) {
+      echo "<br><br><a href=" . "'?link=" . $row["entryid"] . "'" . "<h3>Date Posted:" . $row["dateposted"]."</h3></a><br>";
+      if(isset($_GET['link'])){
+        //echo "uh";
+        $_SESSION['entryid'] = $_GET['link'];
+        header("Location: details.php");
+      }
+    }
+  }
+}
+?>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
